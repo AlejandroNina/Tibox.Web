0784b17715ba0aa8482497154b1460c46ca6b214
Alejandro Nina <Tibox@CL02TB06>
2017-05-25 23:50:33 -0500

    Avance práctica

---
+++ Tibox.Automation.Tests/ProductPageTestNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tibox.Automation.Tests
+{
+    public class ProductPageTestNavigation
+    {
+        public ProductPageTestNavigation()
+        {
+            Driver.GetInstance(DriversOption.Chrome);
+        }
+
+        [Fact]
+        public void ProductTest()
+        {
+            ProductPage.Go();
+            ProductPage.Create().WithDataProduct(20,"Product1",2,160,"Coins",false).Save()
+        }
+    }
+}
---
+++ Tibox.Automation/ProductPage.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tibox.Automation
+{
+    public class ProductPage
+    {
+        public static void Go()
+        {
+            Driver.Instance.Navigate().GoToUrl("http://localhost/Tibox.Angular/");
+            Driver.Instance.FindElement(By.CssSelector("a[href='#!product']")).Click();
+        }
+
+        public static ProductCommand Create()
+        {
+            return new ProductCommand();
+        }
+    }
+
+    public class ProductCommand
+    {
+        private string Id;
+        private string ProductName;
+        private string SupplierId;
+        private string UnitPrice;
+        private string Package;
+        private string IsDiscontinued;
+
+        public ProductCommand()
+        {
+
+        }
+
+        public ProductCommand WithDataProduct(int Id, string ProductName, int SupplierId,
+            decimal UnitPrice, string Package, bool IsDiscontinued)
+        {
+            this.Id = Id.ToString();
+            this.ProductName = ProductName;
+            this.SupplierId = SupplierId.ToString();
+            this.UnitPrice = UnitPrice.ToString();
+            this.Package = Package;
+            this.IsDiscontinued = IsDiscontinued.ToString();
+            return this;
+        }
+
+        public void Save()
+        {
+            Driver.Instance.FindElement(By.Id("Id")).Clear();
+            Driver.Instance.FindElement(By.Id("Id")).SendKeys(Id);
+
+            Driver.Instance.FindElement(By.Id("ProductName")).Clear();
+            Driver.Instance.FindElement(By.Id("ProductName")).SendKeys(ProductName);
+
+            Driver.Instance.FindElement(By.Id("SupplierId")).Clear();
+            Driver.Instance.FindElement(By.Id("SupplierId")).SendKeys(SupplierId);
+
+            Driver.Instance.FindElement(By.Id("UnitPrice")).Clear();
+            Driver.Instance.FindElement(By.Id("UnitPrice")).SendKeys(UnitPrice);
+
+            Driver.Instance.FindElement(By.Id("Package")).Clear();
+            Driver.Instance.FindElement(By.Id("Package")).SendKeys(Package);
+
+            Driver.Instance.FindElement(By.Id("IsDiscontinued")).Clear();
+            Driver.Instance.FindElement(By.Id("IsDiscontinued")).SendKeys(IsDiscontinued);
+
+            Driver.Instance.FindElement(By.CssSelector("button.btn.btn-save")).Click();
+        }
+    }
+}
